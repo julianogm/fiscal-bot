1e8fbf504fab2a795d0fb9769159b9af6600e8d8
juliano <juliano@pop-os.localdomain>
2022-08-07 23:49:16 -0300

    adicionando requisitos para deploy

--- bot.py
+++ bot.py
@@ -158,11 +158,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-    #updater.start_webhook(listen="0.0.0.0",
-    #                        port=int(PORT),
-    #                        url_path=TOKEN,
-    #                        webhook_url = 'https://fiscal-bot.herokuapp.com/' + TOKEN)
+    #updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                            port=int(PORT),
+                            url_path=TOKEN,
+                            webhook_url = 'https://fiscal-bot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
