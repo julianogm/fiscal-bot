f921ebd58160d834cdcb0eb30c69a1229e946549
Juliano Marques <julianoaugusto22@gmail.com>
2022-09-21 19:55:52 -0300

    build: change start method

--- bot.py
+++ bot.py
@@ -176,11 +176,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-    #updater.start_webhook(listen="0.0.0.0",
-    #                        port=int(PORT),
-    #                        url_path=TOKEN,
-    #                        webhook_url = WEBHOOK_URL + TOKEN)
+    #updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                            port=int(PORT),
+                            url_path=TOKEN,
+                            webhook_url = WEBHOOK_URL + TOKEN)
 
     updater.idle()
 
