60f625bb6f2d950564db0733c1f3553def17b3fc
Juliano Marques <juliano.marques@easyb2b.io>
2023-11-18 19:05:47 -0300

    refactor: add deputies and senators classes

--- README.md
+++ README.md
@@ -5,14 +5,15 @@ Bem vindo ao Bot Fiscal.
 Esse é um bot do telegram de consultas as apis da [câmara do deputados](https://dadosabertos.camara.leg.br/swagger/api.html) e do [senado federal](https://legis.senado.leg.br/dadosabertos/docs/ui/index.html) do Brasil.
 
 Você pode testar o bot pesquisando @fiscal_politico_bot no telegram, ou clicando [aqui](http://t.me/fiscal_politico_bot).
-
-  
 _______________
-### Pacotes utilizados:
+### Ferramentas utilizadas:
+
+python 3.10.12
+
 cssselect 1.1.0 \
 lxml 4.9.1 \
 python-dotenv 0.20.0 \
-python_telegram_bot 13.13 \
+python_telegram_bot 13.14 \
 requests 2.25.1 \
 telegram 0.0.1
 
---
+++ apis/senators.py
@@ -0,0 +1,133 @@
+import os
+import sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from datetime import date
+
+import lxml.html
+import requests
+
+from constant import *
+
+CSS_CEAP = "#collapse-ceaps > div:nth-child(1) > table:nth-child(1) > tfoot:nth-child(4) > tr:nth-child(1) > td:nth-child(2)"
+CSS_TELEFONE = ".dl-horizontal > dd:nth-child(10)"
+
+
+class Senators:
+    def __init__(self):
+        pass
+
+    def _get_data(self, url):
+        response = requests.get(url, headers={"Accept": "application/json"})
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            raise Exception(
+                f"Request failed with status {response.status_code}: {response.json()['title']}"
+            )
+
+    def list_senators(self):
+        url = f"{API_SENADO}senador/lista/atual"
+        senators = self._get_data(url)["ListaParlamentarEmExercicio"]["Parlamentares"][
+            "Parlamentar"
+        ]
+        return senators
+
+    def by_state(self, state_acronym):
+        senators = self.list_senators()
+        senators_by_state = [
+            senator
+            for senator in senators
+            if senator["IdentificacaoParlamentar"]["UfParlamentar"] == state_acronym
+        ]
+
+        return senators_by_state
+
+    def by_political_party(self, party_acronym):
+        senators = self.list_senators()
+        senators_by_party = [
+            senator
+            for senator in senators
+            if senator["IdentificacaoParlamentar"]["SiglaPartidoParlamentar"]
+            == party_acronym
+        ]
+
+        return senators_by_party
+
+    def list_political_parties(self):
+        senators = self.list_senators()
+        political_parties = [
+            senator["IdentificacaoParlamentar"]["SiglaPartidoParlamentar"]
+            for senator in senators
+        ]
+        return political_parties
+
+    def by_id(self, senator_id):
+        url = f"{API_SENADO}senador/{senator_id}"
+        senator = self._get_data(url)["DetalheParlamentar"]["Parlamentar"]
+
+        return senator
+
+    def get_senator_data(self, senator_id):
+        senator_data = self._message(senator_id)
+        return senator_data
+
+    # Method responsible for fetch data from federal senate website via webscraping
+    def _senator_info(self, senator_id):
+        current_year = date.today().year
+        url = f"{SITE_SENADO}{senator_id}?ano={current_year}"
+        response = requests.get(url)
+
+        tree = lxml.html.fromstring(response.text)
+        site_info = {}
+
+        if tree.cssselect(CSS_CEAP):
+            ceap_spending = tree.cssselect(CSS_CEAP)[0].text_content()
+            site_info["ceap"] = ceap_spending
+        else:
+            site_info["ceap"] = "Ainda não há gasto registrado nesse ano"
+
+        if tree.cssselect(CSS_TELEFONE):
+            phone = tree.cssselect(CSS_TELEFONE)[0].text_content()
+            site_info["telefone"] = phone[:14]
+        else:
+            site_info["telefone"] = "Número não encontrado"
+
+        return site_info
+
+    def _message(self, senator_id):
+        current_year = date.today().year
+        url = f"{API_SENADO}senador/{senator_id}"
+        senator = self._get_data(url)["DetalheParlamentar"]["Parlamentar"]
+
+        senator_api_data = senator["IdentificacaoParlamentar"]
+        senator_site_data = self._senator_info(senator_id)
+
+        full_name = senator_api_data["NomeCompletoParlamentar"]
+        name_search_jusbrasil = (
+            full_name.replace(" ", "+").translate(NORMALIZAR).lower()
+        )
+
+        senator_email = (
+            senator_api_data.get("EmailParlamentar")
+            if senator_api_data.get("EmailParlamentar") != None
+            else "Sem email cadastrado"
+        )
+
+        message = (
+            f"Nome civil: {full_name}\n"
+            f"Partido: {senator_api_data['SiglaPartidoParlamentar']} | Estado: {senator_api_data['UfParlamentar']}\n"
+            f"Email: {senator_email}\n"
+            f"Telefone: {senator_site_data['telefone']}\n\n"
+            f"Gastos de {senator_api_data['NomeParlamentar']} em {current_year}\n"
+            f"CEAPS: {senator_site_data['ceap']}\n\n"
+            f"Mais sobre o senador(a): {senator_api_data['UrlPaginaParlamentar']}\n"
+            f"https://www.jusbrasil.com.br/artigos-noticias/busca?q={name_search_jusbrasil}\n"
+            f"{senator_api_data['UrlFotoParlamentar']}"
+        )
+
+        return message
